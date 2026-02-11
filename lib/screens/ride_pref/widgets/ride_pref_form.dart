734778c20702ae3ce0cabedf6f7c11df49d85de4
leang-dot <ounleang180@gmail.com>
2026-02-12 06:42:26 +0700

    BLA-003 – Implement Form

--- lib/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
- 
 import '../../../model/ride/locations.dart';
 import '../../../model/ride_pref/ride_pref.dart';
- 
+import 'package:blabla/dummy_data/dummy_data.dart';
+import 'package:blabla/widgets/actions/bla_button.dart';
+import 'package:blabla/theme/theme.dart';
+import '../../../widgets/display/bla_divider.dart';
+
 ///
 /// A Ride Preference From is a view to select:
 ///   - A depcarture location
@@ -24,31 +27,37 @@ class RidePrefForm extends StatefulWidget {
 
 class _RidePrefFormState extends State<RidePrefForm> {
   Location? departure;
-  late DateTime departureDate;
+  DateTime? departureDate;
   Location? arrival;
   late int requestedSeats;
-
-
 
   // ----------------------------------
   // Initialize the Form attributes
   // ----------------------------------
 
+  void initialValue() {
+    if (widget.initRidePref != null) {
+      departure = widget.initRidePref!.departure;
+      departureDate = widget.initRidePref?.departureDate ?? DateTime.now();
+      arrival = widget.initRidePref!.arrival;
+      requestedSeats = widget.initRidePref!.requestedSeats;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
-    // TODO 
+    // TODO
+    initialValue();
   }
 
   // ----------------------------------
   // Handle events
   // ----------------------------------
- 
 
   // ----------------------------------
   // Compute the widgets rendering
   // ----------------------------------
-  
 
   // ----------------------------------
   // Build the widgets
@@ -56,10 +65,154 @@ class _RidePrefFormState extends State<RidePrefForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [ 
- 
-        ]);
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        _departureWidget(),
+        BlaDivider(),
+        _arrivalWidget(),
+        BlaDivider(),
+        _datePickerWidget(context),
+        BlaDivider(),
+        _requestSeatWidget(),
+        BlaButton(color: BlaColors.primary, label: "Search"),
+      ],
+    );
+  }
+
+  Widget _requestSeatWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(padding: const EdgeInsets.all(8.0), child: Icon(Icons.person)),
+        Expanded(
+          child: TextField(
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              focusedBorder: InputBorder.none,
+            ),
+            onChanged: (value) {
+              int result = int.parse(value);
+              setState(() {
+                requestedSeats = result;
+              });
+            },
+            keyboardType: TextInputType.numberWithOptions(),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void pickDate(BuildContext context) async {
+    final DateTime? pickedDate = await showDatePicker(
+      context: context,
+      initialDate: departureDate,
+      firstDate: DateTime(2000),
+      lastDate: DateTime(2100),
+    );
+
+    if (pickedDate != null && pickedDate != departureDate) {
+      setState(() {
+        departureDate = pickedDate;
+      });
+    }
+  }
+
+  Widget _datePickerWidget(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.calendar_month),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            pickDate(context);
+          },
+          child: Text(
+            departureDate == null
+                ? "Select Date"
+                : '${departureDate!.weekday} / ${departureDate!.day} / ${departureDate!.month}',
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _departureWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.location_on_rounded),
+        ),
+        DropdownButton(
+          hint: Text(
+            "Leaving From",
+            style: BlaTextStyles.label.copyWith(color: BlaColors.greyLight),
+          ),
+          icon: Container(),
+          underline: Container(),
+          value: departure,
+          items: [
+            ...fakeLocations.map((element) {
+              return DropdownMenuItem(
+                value: element,
+                child: Text(
+                  element.name,
+                  style: BlaTextStyles.label.copyWith(color: BlaColors.primary),
+                ),
+              );
+            }),
+          ],
+          onChanged: (value) {
+            setState(() {
+              departure = value;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Widget _arrivalWidget() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(Icons.location_on_rounded),
+        ),
+        DropdownButton(
+          hint: Text(
+            "Going To",
+            style: BlaTextStyles.label.copyWith(color: BlaColors.greyLight),
+          ),
+          icon: Container(),
+          underline: Container(),
+          value: arrival,
+          items: [
+            ...fakeLocations.map((element) {
+              return DropdownMenuItem(
+                value: element,
+                child: Text(
+                  "${element.name} , ${element.country.name}",
+                  style: BlaTextStyles.label.copyWith(color: BlaColors.primary),
+                ),
+              );
+            }),
+          ],
+          onChanged: (value) {
+            setState(() {
+              arrival = value;
+            });
+          },
+        ),
+      ],
+    );
   }
 }
